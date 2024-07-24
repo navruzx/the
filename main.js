e0b9c1ae883e4ff4ece6cd6564c26a683a4d276e
navruzx <nnavruz095@gmail.com>
2024-07-24 17:53:14 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,219 @@
+
+// function kv(a) {
+//     let p = 4 * a;
+//     console.log(p);
+//     return p;
+  
+// }
+// kv(5)
+// let kv = (a) => {
+//     let p = 4 * a;
+//     console.log(p);
+//     return p;
+// }
+// kv(4)
+// let kv = function (a) {
+//     let p = 4 * a;
+//     console.log(p);
+//     return p;
+// }
+// kv(10)
+// function s(a) {
+//     let s = 2 * a;
+//     console.log(s);
+//     return s;
+
+// }
+// s(9)
+// let s = (a) => {
+//     let s = 2 * a;
+//     console.log(s);
+//     return s;
+// }
+// s(12)
+// let s = function (a) {
+//     let s = 2 * a;
+//     console.log(s);
+//     return s;
+// }
+// s(10)
+// function l(d) {
+//     let p = 3.14;
+//     let l = p * d;
+//     console.log(l);
+//     return l;
+// }
+// l(9)
+// let l = (d) => {
+//     let p = 3.14;
+//     let l = p * d;
+//     console.log(l);
+//     return l;
+// }
+// l(23)
+// let l = function (d){
+//     // let p = 3.14;
+//     let l = p * d;
+    // console.log(l);
+//     return l;
+// }
+// l(19)
+// function v(a) {
+//     let v = a**3;
+//     let a2 = a**2;
+//     let s = 6 * a2;
+//     console.log(v);
+//     console.log(s);
+//     return v,s;
+// }
+// v(3)
+// let v = (a) => {
+//     let v = a**3;
+//     let a2 = a**2;
+//     let s = 6 * a2;
+//     console.log(v);
+//     console.log(s);
+//     return v,s;
+// }
+// v(4)
+// let v = function(a) {
+//     let v = a**3;
+//     let a2 = a**2;
+//     let s = 6 * a2;
+//     console.log(v);
+//     console.log(s);
+//     return v,s;
+// }
+// v(9)
+// function v(a,b,c) {
+//     let v = a*b*c;
+//     let s = 2 * a * b + b * c + a * c;
+//     console.log(v);
+//     console.log(c);
+//     return v,c;
+// }
+// v(8,8,8)
+// let v = (a,b,c) => {
+//     let v = a*b*c;
+//         let s = 2 * a * b + b * c + a * c;
+//         console.log(v);
+//         console.log(c);
+//         return v,c;
+// }
+// v(2,3,5)
+// let v = function (a,b,c) {
+//     let v = a*b*c;
+//         let s = 2 * a * b + b * c + a * c;
+//         console.log(v);
+//         console.log(c);
+//         return v,c;
+// }
+// v(2,42,43)
+// function l(p,r) {
+//     let l = 2 * p * r;
+//     let r2 = r**2;
+//     let s = p * r2;
+//     console.log(l);
+//     console.log(s);
+//     return l,s;
+// }
+// l(3,9)
+// let l = (p,r) => {
+//     let l = 2 * p * r;
+//     let r2 = r**2;
+//     let s = p * r2;
+//     console.log(l);
+//     console.log(s);
+//     return l,s;
+// }
+// l(8,8)
+// let l = function(p,r) {
+//     let l = 2 * p * r;
+//         let r2 = r**2;
+//         let s = p * r2;
+//         console.log(l);
+//         console.log(s);
+//         return l,s;
+// }
+// l(8,8)
+// function u(a,b){
+//     let u = a*b/2;
+//     console.log(u);
+//     return u;
+// }
+// u(12,9)
+// let l = (a,b) => {
+//     let u = a * b/2;
+//     console.log(u);
+//     return u;
+// }
+// l(3,3)
+// let l = function(a,b){
+//     let u = a * b/2;
+//     console.log(u);
+//     return u;
+// }
+// l(9,9)
+// function a(a,b) {
+//     let o = a*b*2/0.5;
+//     console.log(o);
+//     return o;
+// }
+// a(5,5)
+// let a = (a,b) => {
+//     let o = a*b*2/0.5;
+//     console.log(o);
+//     return o;
+// }
+// a(9,9)
+// let a = function(a,b) {
+//     let o = a*b*2/0.5;
+//     console.log(o);
+//     return o;
+// }
+// a(4,6)
+// function p (a,b) {
+//     let u = a+b;
+//     let k = a*b;
+//     let a2 = a**2;
+//     let b2 = b**2;
+//     console.log(u);
+//     console.log(k);
+//     console.log(a2);
+//     console.log(b2);
+//     return u,k,a2,b2;
+// }
+// p(2,3)
+// let p = (a,b) => {
+//     let u = a+b;
+//     let k = a*b;
+//     let a2 = a**2;
+//     let b2 = b**2;
+//     console.log(u);
+//     console.log(k);
+//     console.log(a2);
+//     console.log(b2);
+//     return u,k,a2,b2;
+// }
+// p(2,22)
+// let p = function(a,b){
+//     let u = a+b;
+//     let k = a*b;
+//     let a2 = a**2;
+//     let b2 = b**2;
+//     console.log(u);
+//     console.log(k);
+//     console.log(a2);
+//     console.log(b2);
+//     return u,k,a2,b2;
+// }
+// p(8,8)
+// function p (a,b) {
+//     let u = a+b;
+//     let k = a*b;
+//     let m1 = a>0;
+//     let m2 = b>0;
+//     console.log(u,k,m1,m2);
+//     return u,k,m1,m2;
+// }
+// p(2,2)
